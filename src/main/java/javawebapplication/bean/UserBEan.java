52b7a71ec2a3eb77a7993f45ae12d90f5168c73e
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-03-03 20:32:57 +0700

    create UserBean

---
+++ src/main/java/javawebapplication/bean/UserBEan.java
@@ -0,0 +1,58 @@
+package javawebapplication.bean;
+
+import java.util.Date;
+
+public class UserBEan {
+	private String firstName;
+	private String lastName;
+	private String login;
+	private String password;
+	private Date dob;
+	private String mobileNo;
+	private long id;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+}
